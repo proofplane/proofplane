c2af9088fd2235f3c8586a71d3e9244646fe65c9
Manrique Iriarte <m_iriarte33@yahoo.com>
2026-08-02 11:15:19 -0400

    Strengthen machine grant snapshot coverage

--- src/repository/agent_policy_document_upload_grants.rs
+++ src/repository/agent_policy_document_upload_grants.rs
@@ -132,6 +132,17 @@ workspace_snapshot_record! {
     scope: workspace_id,
 }
 
+#[cfg(test)]
+mod tests {
+    use super::{GET_FOR_UPDATE_SQL, GET_SQL};
+
+    #[test]
+    fn verification_and_transactional_reads_have_distinct_locking_sql() {
+        assert!(!GET_SQL.contains("FOR UPDATE"));
+        assert!(GET_FOR_UPDATE_SQL.contains("FOR UPDATE"));
+    }
+}
+
 impl TryFrom<Row> for GrantRecord {
     type Error = Error;
 
