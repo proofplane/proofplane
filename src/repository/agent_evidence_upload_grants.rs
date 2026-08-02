bebe2b0c2aa36b9b65b014c206419a1da7b744af
Manrique Iriarte <m_iriarte33@yahoo.com>
2026-08-02 12:35:30 -0400

    Order snapshot repository tests last

--- src/repository/agent_evidence_upload_grants.rs
+++ src/repository/agent_evidence_upload_grants.rs
@@ -131,17 +131,6 @@ workspace_snapshot_record! {
     scope: workspace_id,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{GET_FOR_UPDATE_SQL, GET_SQL};
-
-    #[test]
-    fn verification_and_transactional_reads_have_distinct_locking_sql() {
-        assert!(!GET_SQL.contains("FOR UPDATE"));
-        assert!(GET_FOR_UPDATE_SQL.contains("FOR UPDATE"));
-    }
-}
-
 impl TryFrom<Row> for GrantRecord {
     type Error = Error;
 
@@ -238,5 +227,16 @@ impl TryFrom<&AgentEvidenceUploadGrant> for GrantRecord {
             completed_at: grant.completed_at(),
             document_id: grant.document_id().map(Uuid::from),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{GET_FOR_UPDATE_SQL, GET_SQL};
+
+    #[test]
+    fn verification_and_transactional_reads_have_distinct_locking_sql() {
+        assert!(!GET_SQL.contains("FOR UPDATE"));
+        assert!(GET_FOR_UPDATE_SQL.contains("FOR UPDATE"));
     }
 }
